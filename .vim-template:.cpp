1d353841b48ec5bb3585d631521f0a623eccb978
Nico Ekkart <nicoekkart@gmail.com>
2016-08-26 19:16:26 +0200

    Now correct versions

--- .vim-template:.cpp
+++ .vim-template:.cpp
@@ -1,1 +1,14 @@
-.vim-template:.cpp
+#include <bits/stdc++.h>
+#define EPS 1e-9
+#define INF 1e9
+using namespace std;
+typedef vector<int> vi;
+typedef pair<int,int> ii;
+typedef long long int int64;
+typedef unsigned long long int64u;
+
+int main(){
+    std::ios::sync_with_stdio(false);
+
+    return 0;
+}
